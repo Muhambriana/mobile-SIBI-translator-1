92718123d049db353499eb74f91d770ae7890c1d
Mochammad Lanang Afkaar Arrachmandhika <afkaar2012@live.com>
2021-05-30 06:52:43 +0700

    Update main.py for Flask deployment and add Test JSON for testing the flask

--- main.py
+++ main.py
@@ -1,91 +1,85 @@
-# Impoer the needed File for prediction
-from flask import Flask
+# Impoer the needed modules for prediction
+from flask import Flask, request
 import os
 import tensorflow as tf
 import numpy as np
+import json
 
+# Check for GPU Support and set it to True
+gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
+# Define Classes in Dictionary and Load Created Model Previously
+classes = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8,
+    'J': 9,
+    'K': 10,
+    'L': 11,
+    'M': 12,
+    'N': 13,
+    'O': 14,
+    'P': 15,
+    'Q': 16,
+    'R': 17,
+    'S': 18,
+    'T': 19,
+    'U': 20,
+    'V': 21,
+    'W': 22,
+    'X': 23,
+    'Y': 24,
+    'Z': 25}
+
+# Load Model and show the Summary of the Model
+model = tf.keras.models.load_model('model_SIBI.h5')
+model.summary()
+
+# Check for GPU Support and set it to True
+gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+
+# Define the Flask App
 app = Flask(__name__)
 
-### route dari html-nya nanti
-@app.route('/', methods=['POST'])
-def translator():
-    gpu_devices = tf.config.experimental.list_physical_devices("GPU")
-    for device in gpu_devices:
-        tf.config.experimental.set_memory_growth(device, True)
+### route from JSON android
+@app.route('/predict', methods=['POST'])
+def predict():
+    #If there's an input from Android
+    request_json = request.json
+    print("data: {}".format(request_json))
+    print("type: {}".format(type(request_json)))
 
-    #Define Classes in Dictionary and Load Created Model Previously
-    classes = {
-        'A': 0,
-        'B': 1,
-        'C': 2,
-        'D': 3,
-        'E': 4,
-        'F': 5,
-        'G': 6,
-        'H': 7,
-        'I': 8,
-        'J': 9,
-        'K': 10,
-        'L': 11,
-        'M': 12,
-        'N': 13,
-        'O': 14,
-        'P': 15,
-        'Q': 16,
-        'R': 17,
-        'S': 18,
-        'T': 19,
-        'U': 20,
-        'V': 21,
-        'W': 22,
-        'X': 23,
-        'Y': 24,
-        'Z': 25
+    # Convert into a Array for Prediction
+    IMG_array = np.array(request_json.get('data'))
+
+    # Feed input Array Into the Prediction
+    predictions = model.predict(IMG_array)
+    
+    # Using numpy.argmax to find which class between 26 classes
+    # have the highest probability between them
+    # and Find classes using keys that found before in numpy.argmax
+    for alphabets, values in classes.items():
+        if values == np.argmax(predictions):
+            out_val = alphabets
+    
+    # Edit Received JSON file to response to the Android with prediction
+    response_json = {
+        "data" : request_json.get("data"),
+        "prediction" : str(out_val)
     }
 
-    model = tf.keras.models.load_model('model_SIBI.h5')
-    model.summary()
+    # Send Back the Prediction
+    return json.dumps(response_json)
 
-    #Define the Input for the Inference
-    # Thumb Finger or Jari Jempol
-    thumb_fingerX = 387.35830783843994
-    thumb_fingerY = 854.7810316085815
-    # Index Finger or Jari Telunjuk
-    index_fingerX = 924.9814748764038
-    index_fingerY = 324.4127631187439
-    # Middle Finger or Jari Tengah
-    middle_fingerX = 1076.1100053787231
-    middle_fingerY = 1152.2197723388672
-    # Ring Finger or Jari Manis
-    ring_fingerX = 1185.478687286377
-    ring_fingerY = 1216.181993484497
-    # Pinky Finger or Jari Kelingking
-    pinky_fingerX = 1310.7153177261353
-    pinky_fingerY = 1211.7106914520264
-
-    while True: #Selama Apps Android Terkoneksi dengan Cloud
-        #Kalau ada Input dari Android
-        #Convert into a Array for Prediction
-        input_IMG = [[[thumb_fingerX], [thumb_fingerY],
-                  [index_fingerX], [index_fingerY],
-                  [middle_fingerX], [middle_fingerY],
-                  [ring_fingerX], [ring_fingerY],
-                  [pinky_fingerX], [pinky_fingerY]]]
-        IMG_array = np.array(input_IMG)
-
-        #Feed input Array Into the Prediction
-        predictions = model.predict_classes(IMG_array)
-        for alphabets, values in classes.items():
-            if values == predictions[0] :
-                text_prediction = alphabets
-
-        #Send Prediksi kembali ke Android berupa satu value yaitu text_prediction
-        #text_prediction to Android
-        #Kalau Apps android matiin koneksi, matiin Pythonnya
-    #lihat output dari code untuk cek jalan bener atau enggak.
-
-    return(text_prediction)
-
-if __name__ == "__main__"
-app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ test_prediction_json.py
@@ -0,0 +1,28 @@
+import os
+import json
+import requests
+import numpy as np
+
+# Input Test of 10 x 1 array
+input_test = [[[ 892.317], [ 881.301], 
+               [ 743.465], [ 666.988],
+               [ 885.235], [ 691.533],
+               [1027.745], [ 737.426],
+               [1131.495], [ 753.244]]]
+
+# Create Json File for dumping to the server
+data = json.dumps({"data": input_test})
+
+# Checking inside the dictionary of data inside of the JSON
+print(data)
+
+# Add header to JSON so the server recognize as an JSON Application Request
+headers = {"content-type": "application/json"}
+
+# Send Request to the Server for the Prediction
+json_response = requests.post('http://localhost:5000/predict', data=data, headers=headers)
+
+# Received the response and print the prediction
+prediction = json.loads(json_response.text)['prediction']
+print(prediction)
+
